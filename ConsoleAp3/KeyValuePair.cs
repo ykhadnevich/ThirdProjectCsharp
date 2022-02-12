b1cc5e99fa6174368d25ad6a513a5c1348f267e0
Yan_chik <90506265+ykhadnevich@users.noreply.github.com>
2022-02-12 15:02:33 +0200

    First Commit

---
+++ ConsoleAp3/KeyValuePair.cs
@@ -0,0 +1,89 @@
+﻿namespace ConsoleAp3;
+
+public class KeyValuePair
+{
+    public string Key { get; }
+
+    public string Value { get; }
+
+    public KeyValuePair(string key, string value)
+    {
+        Key = key;
+        Value = value;
+    }
+}
+
+public class LinkedListNode
+{
+    public KeyValuePair Pair { get; }
+        
+    public LinkedListNode Next { get; set; }
+
+    public LinkedListNode(KeyValuePair pair, LinkedListNode next = null)
+    {
+        Pair = pair;
+        Next = next;
+    }
+}
+
+public class LinkedList
+{
+    private LinkedListNode _first;
+
+    public void Add(KeyValuePair pair)
+    {
+        if (_first == null)
+        {
+            _first = new LinkedListNode(pair);
+            return;
+        }
+            
+        var Current = _first;
+        while (Current.Next != null)
+        {
+            Current = Current.Next;
+        }
+
+        Current.Next = new LinkedListNode(pair);
+        
+    }
+
+    public void RemoveByKey(string key)
+    {
+        var Current = _first;
+        if (Current == null) return;
+
+        if (Current.Pair.Key == key) {
+            _first = _first.Next;
+            return;
+        }
+
+        while (Current.Next != null)
+        {
+            if (Current.Next.Pair.Key == key)
+            {
+                Current.Next = Current.Next.Next;
+                return;
+            }
+
+            Current = Current.Next;
+        } 
+    }
+
+    public KeyValuePair GetItemWithKey(string key)
+    {
+        var Current = _first;
+        if (Current == null)
+            return null;
+        do
+        { 
+            if (Current.Pair.Key == key)
+                return Current.Pair;
+            Current = Current.Next;
+        } while (Current != null);
+        
+        
+
+        return null;
+    }
+}
---
+++ ConsoleAp3/Program.cs
@@ -0,0 +1,26 @@
+﻿using ConsoleAp3;
+using KeyValuePair = ConsoleAp3.KeyValuePair;
+
+var list = new LinkedList();
+var lines = File.ReadAllLines("dictionary.txt");
+foreach (var line in lines)
+{
+    var data = line.Split("; ");
+    list.Add(new KeyValuePair(data[0], data[1]));
+}
+while (true)
+{
+    Console.Write("Enter: ");
+    var input = Console.ReadLine().ToUpper();
+    var val = list.GetItemWithKey(input);
+    if (val == null) {
+        Console.WriteLine("Word not found!");
+        continue;
+    }
+
+    Console.WriteLine(val.Value);
+}
+
+
+
+
