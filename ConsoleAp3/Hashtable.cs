53ff764d561d8bb789d164d4125149855db20c82
Yan_chik <90506265+ykhadnevich@users.noreply.github.com>
2022-02-18 11:11:09 +0200

    Update Hashtable.cs
    Add

--- ConsoleAp3/Hashtable.cs
+++ ConsoleAp3/Hashtable.cs
@@ -1,1 +1,31 @@
+﻿namespace ConsoleAp3;
+
+public class StringsDictionary
+{
+    private static int Size = 10000;
+
+    private LinkedList[] _buckets = new LinkedList[Size];
+
+    public void Add(string key, string value)
+    {
+        var index = CalculateHash(key);
+        if (_buckets[index] == null) _buckets[index] = new LinkedList();
+        _buckets[index].Add(new KeyValuePair(key, value));xx
+    }
+
+    public void Remove(string key)
+    {
+        
+    }
+
+    public KeyValuePair Get(string key)
+    {
+        
+    }
+
+    private int CalculateHash(string key)
+    {
+        
+    }
+}
 
